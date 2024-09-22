919c1c7bf2376e4608a804d0ced7508b52dce984
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-09-22 18:59:43 +0000

    ijhghoijhoij

---
+++ task_3.py
@@ -0,0 +1,22 @@
+class Puppy:
+    def __init__(self, states, index):
+        self.states = {'1': 'болеет', '2': 'выздоравливает', '3': 'здоров'}
+        self.index = index
+        self.state = states['1']
+    def get_treatment(self):
+        if self.state == 'болеет':
+            self.state = self.states['2']
+        elif self.state == 'выздоравливает':
+            self.state = self.states['3']
+    def is_healthy(self):
+        if self.state == self.states['3']:
+            print('щенок здоров')
+
+class Dog:
+    def __init__(self, N):
+        pupies = []
+        for i in N:
+            p = Puppy(i)
+        
+
+    
