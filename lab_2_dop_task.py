feebbfd606b77d4e5857744dced93418e82f39a6
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-10-27 08:49:28 +0000

    sdfyhdjkfgdhaskgadsahdaskd

--- lab_2_dop_task.py
+++ lab_2_dop_task.py
@@ -33,7 +33,8 @@ class Warrior:
     def count(cls):
         print(cls.count_ex, '- количество воинов')
         return cls.count_ex
-    
+
+
     def is_dead(self):
         if self.HP <= 0:
             Warrior.count_ex -= 1
@@ -42,12 +43,22 @@ class Warrior:
         else:
             print('Воин пока жив')
 
+
     def atack(self, who):
         if who.HP > 0:
-            who.HP -= Warrior.damage * len()
+            who.HP -= Warrior.damage
             Warrior.sum_damage += Warrior.damage
             if who.HP <= 0:
                 who.is_dead()
+    @staticmethod
+    def selected_atack(selected_units, selected_enemy):
+        if len(selected_enemy) == len(selected_units):
+            for i in range(len(selected_enemy)):
+                selected_units[i].atack(selected_enemy[i])
+        elif len(selected_enemy) > len(selected_units):
+            print('выбрано недостаточно юнитов, один юнит может бить только одного противника')
+        else:
+            print('Выбрано недостаточно противников, их не хватает, чтобы мог участвовать каждый выбранный юнит')
 
     @classmethod
     def improve_damage(cls):
@@ -62,9 +73,9 @@ class Warrior:
         print(f'Общий нанесённый воинами урон: {Warrior.sum_damage}')
 
     
-    def info_about_unit(self, unit):
-        print(f'{unit.HP} - здоровье')
-        print(f'{unit.damage} - урон')
+    def info_about_unit(self):
+        print(f'{self.HP} - здоровье')
+        print(f'{self.damage} - урон')
 
     @classmethod
     def info_about_class(cls):
@@ -110,6 +121,28 @@ class Mage:
         else:
             print('Хватит бить мёртвых')
 
+    
+    @staticmethod
+    def selected_atack(selected_units, selected_enemy):
+        if len(selected_enemy) == len(selected_units):
+            for i in range(len(selected_enemy)):
+                selected_units[i].atack(selected_enemy[i])
+        elif len(selected_enemy) > len(selected_units):
+            print('выбрано недостаточно юнитов, один юнит может бить только одного противника')
+        else:
+            print('Выбрано недостаточно противников, их не хватает, чтобы мог участвовать каждый выбранный юнит')
+
+
+    def meteorite_atack(self, selected_enemy):
+        for i in range(len(selected_enemy)):
+            if selected_enemy[i].HP > 0:
+                selected_enemy[i].HP -= 500
+                if selected_enemy[i].HP <= 0:
+                    selected_enemy[i].is_dead()
+                Mage.sum_damage += 500
+            else:
+                print('Хватит бить мёртвых')
+
     @staticmethod
     def create_skeleton(skeletons):
         skeleton = Skeleton()
@@ -122,9 +155,10 @@ class Mage:
         print(f'Общий нанесённый магами урон: {Mage.sum_damage}')
 
     
-    def info_about_unit(self, unit):
-        print(f'{unit.HP} - здоровье')
-        print(f'{unit.damage} - урон')
+    def info_about_unit(self):
+        print(f'{self.HP} - здоровье')
+        print(f'{self.damage} - урон')
+
 
     @classmethod
     def info_about_class(cls):
@@ -166,7 +200,18 @@ class Skeleton:
             if who.HP <= 0:
                 who.is_dead()
 
+
+    @staticmethod
+    def selected_atack(selected_units, selected_enemy):
+        if len(selected_enemy) == len(selected_units):
+            for i in range(len(selected_enemy)):
+                selected_units[i].atack(selected_enemy[i])
+        elif len(selected_enemy) > len(selected_units):
+            print('выбрано недостаточно юнитов, один юнит может бить только одного противника')
+        else:
+            print('Выбрано недостаточно противников, их не хватает, чтобы мог участвовать каждый выбранный юнит')
     
+
     def army_atack(self, who):
         for _ in range(Skeleton.count_ex):
             self.atack(who)
@@ -177,10 +222,11 @@ class Skeleton:
         print(f'Общий нанесённый скелетами урон: {Skeleton.sum_damage}')
 
 
-    def info_about_unit(self, unit):
-        print(f'{unit.HP} - здоровье')
-        print(f'{unit.damage} - урон')
-
+    def info_about_unit(self):
+        print(f'{self.HP} - здоровье')
+        print(f'{self.damage} - урон')
+    
+    
     @classmethod
     def info_about_class(cls):
         print(f'{cls.count_ex} - всего юнитов класса {cls}')
@@ -197,9 +243,9 @@ def spawn_unit(unit_class, class_list):
     unit = unit_class()
     class_list.append(unit)
 
-
-spawn_unit(Warrior, Warrior_list)
-spawn_unit(Mage, Mage_list)
+for _ in range(10):
+    spawn_unit(Warrior, Warrior_list)
+    spawn_unit(Mage, Mage_list)
 
 for _ in range(3):
     Warrior_list[0].atack(Mage_list[0])
@@ -213,13 +259,23 @@ Mage.count()
 Skeleton.count()
 
 Warrior.improve_damage()
-info_about_unit(Warrior_list[0])
+Warrior_list[0].info_about_unit()
 Warrior.improve_damage()
-info_about_unit(Warrior_list[0])
+Warrior_list[0].info_about_unit()
 
-Skeleton_list[0].army_atack(Warrior_list[3])
+Skeleton_list[2].army_atack(Warrior_list[3])
 
 Mage_list[2].atack(Skeleton_list[5])
+for _ in range(3):
+    Mage_list[1].meteorite_atack(Warrior_list[1:9])
 
 Warrior.count()
+Warrior_list[3].atack(Skeleton_list[2])
+Warrior.selected_atack(Warrior_list[1:10], Skeleton_list[5:14])
+Skeleton.selected_atack(Skeleton_list[5:8], Mage_list[0:3])
+
+Warrior.info_about_class()
+Mage.info_about_class()
+Skeleton.info_about_class()
+
 
