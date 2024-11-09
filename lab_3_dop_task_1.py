f1d1f6927e36f4ea993b119161aa63268577fc13
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-11-09 11:02:07 +0000

    fgfklxkclvnxcvnxck

--- lab_3_dop_task_1.py
+++ lab_3_dop_task_1.py
@@ -178,58 +178,61 @@ class Skeleton(Unit):
             self.atack(who)
 
 
+if __name__ == '__main__':
 
 
+    Warrior_list = []
+    Mage_list = []
+    Skeleton_list = []
+        
+    def spawn_unit(unit_class, class_list):
+        unit = unit_class()
+        class_list.append(unit)
 
-Warrior_list = []
-Mage_list = []
-Skeleton_list = []
-    
-def spawn_unit(unit_class, class_list):
-    unit = unit_class()
-    class_list.append(unit)
+    for _ in range(10):
+        spawn_unit(Warrior, Warrior_list)
+        spawn_unit(Mage, Mage_list)
 
-for _ in range(10):
-    spawn_unit(Warrior, Warrior_list)
-    spawn_unit(Mage, Mage_list)
+    for _ in range(3):
+        Warrior_list[0].atack(Mage_list[0])
 
-for _ in range(3):
-    Warrior_list[0].atack(Mage_list[0])
+    for _ in range(20):
+        Mage_list[0].create_skeleton(Skeleton_list)
 
-for _ in range(20):
-    Mage_list[0].create_skeleton(Skeleton_list)
+    Warrior.count()
+    Mage.count()
+    Skeleton.count()
 
-Warrior.count()
-Mage.count()
-Skeleton.count()
+    Warrior.improve_damage()
+    Warrior_list[0].info_about_unit()
+    Warrior.improve_damage()
+    Warrior_list[0].info_about_unit()
+    Warrior_list[1].atack(Skeleton_list[1])
 
-Warrior.improve_damage()
-Warrior_list[0].info_about_unit()
-Warrior.improve_damage()
-Warrior_list[0].info_about_unit()
+    Skeleton_list[2].army_atack(Warrior_list[3])
 
-Skeleton_list[2].army_atack(Warrior_list[3])
+    Mage_list[2].atack(Skeleton_list[5])
+    for _ in range(3):
+        Mage_list[1].meteorite_atack(Warrior_list[1:9])
 
-Mage_list[2].atack(Skeleton_list[5])
-for _ in range(3):
-    Mage_list[1].meteorite_atack(Warrior_list[1:9])
+    Warrior.count()
+    Warrior_list[3].atack(Skeleton_list[2])
+    Warrior.selected_atack(Warrior_list[1:10], Skeleton_list[5:14])
+    Skeleton.selected_atack(Skeleton_list[5:8], Mage_list[0:3])
 
-Warrior.count()
-Warrior_list[3].atack(Skeleton_list[2])
-Warrior.selected_atack(Warrior_list[1:10], Skeleton_list[5:14])
-Skeleton.selected_atack(Skeleton_list[5:8], Mage_list[0:3])
+    Warrior.info_about_class()
+    Mage.info_about_class()
+    Skeleton.info_about_class()
 
-Warrior.info_about_class()
-Mage.info_about_class()
-Skeleton.info_about_class()
+    for i in range(len(Warrior_list)):
+        Warrior_list[i].info_about_unit()
+    print('')
 
-for i in range(len(Warrior_list)):
-    Warrior_list[i].info_about_unit()
-print('')
+    for i in range(len(Mage_list)):
+        Mage_list[i].info_about_unit()
+    print('')
 
-for i in range(len(Mage_list)):
-    Mage_list[i].info_about_unit()
-print('')
+    for i in range(len(Skeleton_list)):
+        Skeleton_list[i].info_about_unit()
 
-for i in range(len(Skeleton_list)):
-    Skeleton_list[i].info_about_unit()
+    Warrior_list[9].atack(Skeleton_list[1])
